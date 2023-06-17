a260d5a0000820f79297767415d038808b1a791a
jaire silva <jaire.silva@hotmail.com>
2023-06-17 19:02:49 -0400

    corrigo bug que não permitia usuário acessar paginas privadas caso carregasse a pagina, solicitando novo login

--- src/routes/Private.js
+++ src/routes/Private.js
@@ -6,6 +6,12 @@ function Private({ children }) {
 
     const { signed, loading } = useContext(AuthContext)
 
+    if (loading) {
+        return (
+            <div></div>
+        )
+    }
+
     if (!signed) {
         return <Navigate to="/" />
     }
